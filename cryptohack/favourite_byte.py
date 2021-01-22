887780055743d429329065b14f465d7855d493e8
jssssv007 <64346319+jssssv007@users.noreply.github.com>
2021-01-22 16:56:09 +0530

    Update favourite_byte.py

--- cryptohack/favourite_byte.py
+++ cryptohack/favourite_byte.py
@@ -4,13 +4,13 @@ def sol():
 			k=bytes.fromhex(arr[i]).decode("utf-8")
 			if("crypto" in k):
 				print(k)
-				print(arr[i])
+				#print(arr[i])
 		except:
 			print("",end="")
 x="73626960647f6b206821204f21254f7d694f7624662065622127234f726927756d"
 y=bytes.fromhex(x).decode("utf-8")
 k=bytes(y,'utf-8')
-print(k)
+#print(k)
 arr=[]
 s=b''
 for j in range(1,256):
